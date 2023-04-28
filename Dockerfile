566a02ecdafe752662ba8cbc6d1dcbcbdfc9614f
xaoei <xaoei@outlook.com>
2023-04-28 15:07:21 +0800

    👷Add gunicorn.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
+# 使用 Python 3.8 的 slim 版本作为基础镜像，该镜像已经包含了 Python 环境
 FROM python:3.8-slim-buster
 
-# 安装curl
+# 安装 curl 工具，curl 用于在容器内下载文件等操作
 RUN apt-get update && \
     apt-get install -y curl && \
     rm -rf /var/lib/apt/lists/*
@@ -8,15 +9,18 @@ RUN apt-get update && \
 # 设置工作目录
 WORKDIR /app
 
-# 安装依赖包
+# 将 requirements.txt 文件复制到容器中，并且安装依赖包
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# 复制应用程序代码到容器中
+# 复制应用程序代码到容器中，注意这里是将当前目录下的所有文件都复制到容器中，包括 Dockerfile 和 requirements.txt 等不必要的文件，应该根据实际情况进行调整
 COPY . .
 
-# 声明端口
+# 声明端口，该容器会监听 80 端口
 EXPOSE 80
 
-# 启动应用程序
-CMD ["python", "app.py"]
+# 设置工作目录，注意这里多次设置工作目录的原因是为了让 CMD 命令的执行路径在 /app 目录下，这样可以确保应用程序可以正常启动
+WORKDIR /app
+
+# 启动应用程序，使用 gunicorn 作为 Web 服务器，-w 1 表示只启动一个 worker 进程，-t 60 表示超时时间为 60 秒，-b 0.0.0.0:80 表示监听所有 IP 地址的 80 端口，后面的 app:app 表示应用程序入口为 app.py 文件中名字为 app 的 Flask 实例
+CMD ["/bin/bash", "-c", "gunicorn -w 1 -t 60 -b 0.0.0.0:80 app:app --preload"]
--- app.py
+++ app.py
@@ -5,56 +5,70 @@ import os
 import re
 import argparse
 import subprocess
-from flask import Flask, request
 
+from flask import Flask
+
+# 初始化 Flask 应用程序
 app = Flask(__name__)
 
+# 获取项目目录路径
 project_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 创建解析器对象，并添加命令行参数
+parser = argparse.ArgumentParser(description='Start the nali web service.')
+parser.add_argument('--listen-host', type=str, default=os.getenv('LISTEN_HOST', '0.0.0.0'),
+                    help='The hostname that the server will use. Default is 0.0.0.0')
+parser.add_argument('--listen-port', type=int, default=os.getenv('LISTEN_PORT', 80),
+                    help='The listening port that the server will use. Default is 80')
+parser.add_argument('--nali-update', action='store_true', default=False,
+                    help='Whether to force update nali or not.')
+parser.add_argument('--ip-update', action='store_true', default=False,
+                    help='Whether to force update IP database or not.')
+
+# 解析命令行参数
+# 如果当前运行环境是 gunicorn，使用默认参数启动应用程序
+if 'SERVER_SOFTWARE' in os.environ and os.environ['SERVER_SOFTWARE'].startswith('gunicorn/'):
+    print('Running under Gunicorn!')
+    args = parser.parse_args(args=[])
+else:
+    print('Not running under Gunicorn!')
+    args = parser.parse_args()
+
+# 更新 nali
+if not os.path.exists(f'{project_dir}/scripts/nali') or args.nali_update:
+    subprocess.run(['/bin/bash', 'downloads.sh'], cwd=f'{project_dir}/scripts', stdout=subprocess.PIPE)
+elif args.ip_update:
+    subprocess.run([f'{project_dir}/scripts/nali', 'update'], stdout=subprocess.PIPE)
 
 
 @app.route('/')
 def index():
-    return "ok"
+    """默认路由"""
+    return 'ok'
 
 
 @app.route('/<ip>')
 def nali(ip):
+    """查询 IP 地址信息"""
     if not ip:
         return 'Please provide an IP address.'
-    ips = re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}?)+", ip)
+    # 提取 IP 地址
+    ips = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}?)+', ip)
 
-    cmd_result = subprocess.run([f'{project_dir}/scripts/nali', " ".join(ips)], stdout=subprocess.PIPE)
-    ip_info_str_list = cmd_result.stdout.decode().split("  ")
+    # 调用 nali 查询 IP 地址信息
+    cmd_result = subprocess.run([f'{project_dir}/scripts/nali', ' '.join(ips)], stdout=subprocess.PIPE)
+    ip_info_str_list = cmd_result.stdout.decode().split('  ')
     ip_info_str_list = [(el.strip()) for el in ip_info_str_list]
     result = []
     for ip_info_str in ip_info_str_list:
-        split_columns = ip_info_str.split(" [")
+        split_columns = ip_info_str.split(' [')
         split_columns[1] = split_columns[1].rstrip(']')
         result.append({
             'ip': split_columns[0],
-            "addr": split_columns[1].strip()
+            'addr': split_columns[1].strip()
         })
     return result
 
 
 if __name__ == '__main__':
-    # 创建解析器对象，并添加一个命令行参数
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--listen-host", type=str, help="The hostname that the server will use.",
-                        default=os.getenv("LISTEN_HOST", "0.0.0.0"))
-    parser.add_argument("--listen-port", type=int, help="The listening port that the server will use.",
-                        default=os.getenv("LISTEN_PORT", 80))
-    parser.add_argument("--nali-update", type=bool, help="Nali forced updates.",
-                        default=os.getenv("NALI_UPDATE", False))
-    parser.add_argument("--ip-update", type=bool, help="IP db forced updates.",
-                        default=os.getenv("IP_UPDATE", False))
-    # 解析命令行参数
-    args = parser.parse_args()
-
-    if not os.path.exists(f"{project_dir}/scripts/nali") or args.nali_update:
-        subprocess.run(['/bin/bash', 'downloads.sh'], cwd=f"{project_dir}/scripts", stdout=subprocess.PIPE)
-    elif args.ip_update:
-        # 更新一下
-        subprocess.run([f'{project_dir}/scripts/nali', "update"], stdout=subprocess.PIPE)
-
     app.run(host=args.listen_host, port=args.listen_port)
